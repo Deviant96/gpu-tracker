f29d319a7fced42d67bbf3450926a2fe2f07ff31
deviant96 <mcrealdeviant@gmail.com>
2022-04-17 05:13:12 +0700

    Default code

--- index.php
+++ index.php
@@ -2,27 +2,12 @@
 include "db.php";
 
 try {
-    $stmt = $conn->prepare("SELECT * FROM gpu_data");
-    $stmt->execute();
-    $result = $stmt->fetchAll();
-
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
-
-function grab_specific_gpu(string $gpu_name)
-{
-    global $conn;
-    $results = array();
-    try {
-        $stmt = $conn->prepare("SELECT * FROM url_list LEFT JOIN gpu_data ON gpu_data.gpu_id = url_list.id WHERE url_list.gpu_model = :gpu_name");
-        $stmt->bindParam(':gpu_name', $gpu_name);
-        $stmt->execute();
-        $results = $stmt->fetchAll();
-    } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
-    return $results;
+  $stmt = $conn->prepare("SELECT * FROM gpu_data");
+  $stmt->execute();
+  $result = $stmt->fetchAll();
+  
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
 
 try {
@@ -30,11 +15,12 @@ try {
     //$stmt2->bindParam(':category', "gpu-progress");
     $stmt2->execute();
     $gpuprogress = $stmt2->fetchAll();
-
-} catch (PDOException $e) {
+    
+  } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
-}
+  }
 
+$conn = null;
 ?>
 
 <!DOCTYPE html>
@@ -43,29 +29,12 @@ try {
         <title>Data Harga GPU Dari Beberapa Toko di Tokopedia</title>
         <meta charset="utf-8">
         <link rel="icon" type="image/x-icon" href="favicon.png">
-
+        
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-        <style>
-            .content {
-                display: none;
-            }
-            .show_hide > button.toggle {
-                float: right;
-                font-weight: normal;
-                text-align: right;
-                padding-right: 0.2em;
-                padding-left: 0.2em;
-                cursor: pointer;
-                line-height: 1.5em;
-                border-spacing: 0;
-            }
-        </style>
     </head>
     <body>
     <header class="container">
         <h1>Dashboard Data GPU</h1>
-        <small class="text-muted">Data diupdate setiap jam</small>
     </header>
     <section class="container mt-3">
         <h4>Tambah data baru</h4>
@@ -79,14 +48,11 @@ try {
             </div>
         </form>
         <hr>
-        <button class="btn">Show All</button>
     </section>
-
-    <section class="container-fluid mt-5" style="font-size:70%;">
-        <div class="row">
-            <div class="col">
-            <h5>RX6900XT</h5>
-            <button class="toggle">Show</button>
+    
+    <section class="container mt-5">
+        <h4>Data GPU</h4>
+        <small class="text-muted">Data diupdate setiap jam</small>
         <table class="table table-striped">
             <thead>
                 <tr>
@@ -105,204 +71,48 @@ try {
             </thead>
             <tbody>
             <?php
-            foreach (grab_specific_gpu("RX6900XT") as $data => $val) {
-                if ($val['stock'] > 0) {
-                    echo "<tr>";} else {
-                    echo "<tr class=table-warning>";}
-                echo "<td>" . $val['shopname'] . "</td>";
-                echo "<td>" . $val['title'] . "</td>";
-                if($val['old_price_int']==0) {
-                    echo "<td>N/A</td>";
-                    echo "<td>N/A</td>";
-                } else {
-                    echo "<td>" . $val['old_price'] . " <small class=text-muted>pada " . $val['old_datetime'] . "</small></td>";
-                    echo "<td>" . $val['old_stock'] . "</td>";
-                }
-                echo "<td>";
-                echo $val['latest_price'] . "</td>";
-                echo "<td>" . $val['stock'] . "</td>";
-                echo "<td>" . $val['latest_update_time'] . "</td>";
-                echo "<td>" . date('d-m-Y', strtotime($val['latest_update_date'])) . "</td>";
-                echo "</tr>";
-            }
-            ?>
-            </tbody>
-        </table>
-            </div>
-        <div class="col">
-            <h5>RX6800XT</h5>
-            <button class="toggle">Show</button>
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th rowspan=2 class="align-middle">Toko</th>
-                        <th rowspan=2 class="align-middle">Nama</th>
-                        <th rowspan=2 class="align-middle">Harga Lama</th>
-                        <th rowspan=2 class="align-middle">Stok Lama</th>
-                        <th rowspan=2 class="align-middle">Harga Terbaru</th>
-                        <th rowspan=2 class="align-middle">Stok</th>
-                        <th colspan=2 class="align-middle">Update Terakhir</th>
-                    </tr>
-                    <tr>
-                        <th>Waktu</th>
-                        <th>Tanggal (tgl/bln/thn)</th>
-                    </tr>
-                </thead>
-                <tbody">
-                <?php
-                foreach (grab_specific_gpu("RX6800XT") as $data => $val) {
-                    if ($val['stock'] > 0) {
-                        echo "<tr>";} else {
-                        echo "<tr class=table-warning>";}
-                    echo "<td>" . $val['shopname'] . "</td>";
-                    echo "<td>" . $val['title'] . "</td>";
-                    if($val['old_price_int']==0) {
-                        echo "<td>N/A</td>";
-                        echo "<td>N/A</td>";
-                    } else {
-                        echo "<td>" . $val['old_price'] . " <small class=text-muted>pada " . $val['old_datetime'] . "</small></td>";
-                        echo "<td>" . $val['old_stock'] . "</td>";
-                    }
-                    echo "<td>";
-                    echo $val['latest_price'] . "</td>";
-                    echo "<td>" . $val['stock'] . "</td>";
-                    echo "<td>" . $val['latest_update_time'] . "</td>";
-                    echo "<td>" . date('d-m-Y', strtotime($val['latest_update_date'])) . "</td>";
-                    echo "</tr>";
-                }
-                ?>
-                </tbody>
-            </table>
-        </div>
-        <div class="col">
-            <h5>RX6700XT</h5>
-            <button class="toggle">Show</button>
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th rowspan=2 class="align-middle">Toko</th>
-                        <th rowspan=2 class="align-middle">Nama</th>
-                        <th rowspan=2 class="align-middle">Harga Lama</th>
-                        <th rowspan=2 class="align-middle">Stok Lama</th>
-                        <th rowspan=2 class="align-middle">Harga Terbaru</th>
-                        <th rowspan=2 class="align-middle">Stok</th>
-                        <th colspan=2 class="align-middle">Update Terakhir</th>
-                    </tr>
-                    <tr>
-                        <th>Waktu</th>
-                        <th>Tanggal (tgl/bln/thn)</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <?php
-                foreach (grab_specific_gpu("RX6700XT") as $data => $val) {
-                    if ($val['stock'] > 0) {
-                        echo "<tr>";} else {
-                        echo "<tr class=table-warning>";}
-                    echo "<td>" . $val['shopname'] . "</td>";
-                    echo "<td>" . $val['title'] . "</td>";
-                    if($val['old_price_int']==0) {
-                        echo "<td>N/A</td>";
-                        echo "<td>N/A</td>";
-                    } else {
-                        echo "<td>" . $val['old_price'] . " <small class=text-muted>pada " . $val['old_datetime'] . "</small></td>";
-                        echo "<td>" . $val['old_stock'] . "</td>";
-                    }
-                    echo "<td>";
-                    echo $val['latest_price'] . "</td>";
-                    echo "<td>" . $val['stock'] . "</td>";
-                    echo "<td>" . $val['latest_update_time'] . "</td>";
-                    echo "<td>" . date('d-m-Y', strtotime($val['latest_update_date'])) . "</td>";
-                    echo "</tr>";
-                }
-                ?>
-                </tbody>
-            </table>
-        </div>
-        </div>
-
-        <table class="table-striped">
-            <thead>
-                <tr>
-                    <th rowspan=2 class="align-middle">Toko</th>
-                    <th rowspan=2 class="align-middle">Nama</th>
-                    <th rowspan=2 class="align-middle">Harga Lama</th>
-                    <th rowspan=2 class="align-middle">Stok Lama</th>
-                    <th rowspan=2 class="align-middle">Harga Terbaru</th>
-                    <th rowspan=2 class="align-middle">Stok</th>
-                    <th colspan=2 class="align-middle">Update Terakhir</th>
-                </tr>
-                <tr>
-                    <th>Waktu</th>
-                    <th>Tanggal (tgl/bln/thn)</th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php
+            
             foreach ($result as $data => $val) {
-                if ($val['stock'] > 0) {
-                    echo "<tr>";} else {
+                if($val['stock'] > 0){
+                echo "<tr>";}else{
                     echo "<tr class=table-warning>";}
-                echo "<td>" . $val['shopname'] . "</td>";
-                echo "<td>" . $val['title'] . "</td>";
-                if($val['old_price_int']==0) {
-                    echo "<td>N/A</td>";
-                    echo "<td>N/A</td>";
-                } else {
-                    echo "<td>" . $val['old_price'] . " <small class=text-muted>pada " . $val['old_datetime'] . "</small></td>";
-                    echo "<td>" . $val['old_stock'] . "</td>";
-                }
-                echo "<td>";
-                echo $val['latest_price'] . "</td>";
-                echo "<td>" . $val['stock'] . "</td>";
-                echo "<td>" . $val['latest_update_time'] . "</td>";
-                echo "<td>" . date('d-m-Y', strtotime($val['latest_update_date'])) . "</td>";
+                echo "<td>".$val['shopname']."</td>";
+                echo "<td>".$val['title']."</td>";
+                echo "<td>".$val['old_price']." <small class=text-muted>pada ".$val['old_datetime']."</small></td>";
+                echo "<td>".$val['old_stock']."</td>";
+                //if($val['old_price_int'] == $val['latest_price_int']){
+                    echo "<td>";
+                //} else if($val['old_price'] > $val['latest_price']){
+                //    echo "<td class=table-success>";
+                //} else {
+                //    echo "<td class=table-danger>";
+                //} 
+                echo $val['latest_price']."</td>";
+                echo "<td>".$val['stock']."</td>";
+                echo "<td>".$val['latest_update_time']."</td>";
+                echo "<td>".date('d-m-Y',strtotime($val['latest_update_date']))."</td>";
                 echo "</tr>";
             }
             ?>
             </tbody>
         </table>
-
+        
     </section>
 
     <footer>
-        <?php
-        foreach ($gpuprogress as $data) {
-            echo ":" . $data['date_time'] . " : " . $data['mssg'] . "<br>";
-        }
+        <?php 
+            foreach ($gpuprogress as $data) {
+                echo ":".$data['date_time']." : ".$data['mssg']."<br>";
+            }
         ?>
     </footer>
-
-    <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script>
     $(function () {
-        $('[data-toggle="popover"]').popover()
-
-
+    $('[data-toggle="popover"]').popover()
     })
-
-    $(document).ready(function(){
-        $("table.table").addClass("content");
-    });
-
-    $(".toggle").click (function(){
-        var target = $(this).closest('.col').find('.content');
-
-        if($(this).hasClass('active')) {
-            $(this).removeClass('active');
-            $(this).text("Show");
-            target.stop().slideToggle(0);
-        } else {
-            $(this).addClass('active');
-            $(this).text("Hide");
-            target.stop().slideToggle(0);
-        }
-
-    });
-</script>
-</body>
+</script></body>
 </html>
-
-<?php $conn = null;?>
