1372980baa4f9a334c16fdbdb1cf41f4b8df2994
deviant96 <mcrealdeviant@gmail.com>
2022-04-28 12:00:37 +0700

    Price styling, make it easier to distinguish price, stock, and date

--- ajax_grab_specific_gpu.php
+++ ajax_grab_specific_gpu.php
@@ -17,13 +17,13 @@ try {
                 $html .= "<td data-label='Nama Toko'>" . $row['shopname'] . "</td>";
         $html .= "<td data-label='Nama Item'><a href=" . $row['the_url'] . " target='_blank'><strong>" . $row['title'] . "</strong></a></td>";
         
-        $html .= "<td data-label='Harga'>";
-        $html .= $row['latest_price'] . " (" . $row['stock'] . ")  <small>" . $row['latest_update_time'] . " " . date('d-m-Y', strtotime($row['latest_update_date'])) . "</small></td>";
+        $html .= "<td data-label='Harga'><strong>";
+        $html .= $row['latest_price'] . "</strong> <sup>(" . $row['stock'] . ")</sup>  <span style='font-size:10px;'>" . $row['latest_update_time'] . " " . date('d-m-Y', strtotime($row['latest_update_date'])) . "</span></td>";
         
         if($row['old_price_int']==0) {
             $html .= "<td data-label='Harga Lama'>N/A</td>";
         } else {
-            $html .= "<td data-label='Harga Lama'>" . $row['old_price'] . " (" . $row['old_stock'] . ") <small> " . $row['old_datetime'] . "</small></td>";
+            $html .= "<td data-label='Harga Lama'><strong>" . $row['old_price'] . "</strong> <sup>(" . $row['old_stock'] . ")</sup> <span style='font-size:10px;'> " . $row['old_datetime'] . "</span></td>";
         }
         
         $html .= "</tr>";
